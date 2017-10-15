b57258decd4c442c588a1586d36ad7552cee589f
Darrick <dmckirnan@hotmail.com>
2017-10-15 11:28:44 -0700

    installed package.json

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: '',
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Welcome Home
+      </div>
+    );
+  }
+}
+
+export default App;
