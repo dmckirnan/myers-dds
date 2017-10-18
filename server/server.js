388028dad748c1539296099df2e6c45ab7f0e446
Darrick <dmckirnan@hotmail.com>
2017-10-18 10:46:46 -0700

    added procfile

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 // const port = process.env.port || 5000;
 
 // app.use('/static', express.static(path.join(__dirname, '..', 'build')));
-app.use(express.static(path.join(__dirname, '..', '/')));
-app.get('*', (req, res) => res.sendFile('build/index.html'));
+app.use(express.static(path.join(__dirname, '..', '/build')));
+app.get('*', (req, res) => res.sendFile('index.html'));
 
 app.listen(process.env.port || 5000);
