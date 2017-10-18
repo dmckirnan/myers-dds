a89c93b8d603337299dfb0566deb536e2d7dd188
Darrick <dmckirnan@hotmail.com>
2017-10-18 12:15:14 -0700

    updated server, deploy working

--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-// const port = process.env.port || 5000;
 
-// app.use('/static', express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static(path.join(__dirname, '..', '/build')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'build/index.html')));
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Listening');
 });
