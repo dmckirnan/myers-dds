936009f0c88ac4b6221a8a811f4c2f70c54d352c
Darrick <dmckirnan@hotmail.com>
2017-10-18 11:05:22 -0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,20 @@ const config = {
     inline: true,
     open: true,
   },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
