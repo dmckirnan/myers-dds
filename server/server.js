54c2cc63fc58367499b917f225ba7584b406bf2f
Darrick <dmckirnan@hotmail.com>
2017-10-18 09:54:26 -0700

    updated

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 // const port = process.env.port || 5000;
 
 // app.use('/static', express.static(path.join(__dirname, '..', 'build')));
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '..', '/')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'build/index.html')));
 
 const server = app.listen(process.env.port || 5000, () => console.log('Live on 5000'));
